3bc5972a4350cc6afb12df67a97b492dbbc09867
vienpham <viendev@outlook.com>
2023-06-26 21:42:11 +0700

    fix: update description experiences

--- src/App.tsx
+++ src/App.tsx
@@ -45,14 +45,12 @@ function App() {
               November 2022 - May 2023 <span>// Ho Chi Minh, Vietnam</span>
             </div>
             <ul className="cv-journey__experience-description">
-              <li>Directly collaborated with Product team to deliver new features with implement UI,
-                bind data from API, e2e test, and fix bugs with process: develop, review, and test.
+              <li>Worked closely with the Product team to develop new features for our web application. Created the user interface, integrating data from the backend API, writing end-to-end tests, and fixing bugs.
+                Followed a rigorous process of development, code review, and testing to ensure <br />high-quality deliverables.
               </li>
-              <li>Work with Vue.js 3 by using Nuxt composition API and Nuxt 3, Windicss,
-                Oruga, Pinia, and GraphQL via Nuxt modules that interact with Cubejs, Chartjs,
-                and TypeScript for building Dashboard web application and Chrome extension.
+              <li>Worked with Vue.js 3 by using Nuxt composition API and Nuxt 3, Windicss, Oruga, Pinia, and GraphQL via Nuxt modules that interact with Cubejs, Chartjs, and TypeScript for building Dashboard web application and Chrome extension.
               </li>
-              <li>Working style with daily meeting scrum and planning sprint meeting (2 weeks)</li>
+              <li>Participated in daily meeting scrums to update my team on my progress, challenges and goals. Attended planning sprint meetings every two weeks to review the completed tasks, prioritize the backlog and set the objectives for the next sprint.</li>
             </ul>
           </div>
           {/*archway*/}
@@ -64,23 +62,21 @@ function App() {
             </div>
             <ul className={'cv-journey__experience-description'}>
               <li>
-                Work with various project as Full-stack developer that created project
-                as Prototype/MVP by using Vue.js/Nuxt/Vuetify on Frontend with Backend as same as .Net Core (v3,5,6),
-                Java SpringBoot (Gradle build).
+                Created prototype and MVP projects using various technologies. On the frontend, use Vue.js, Nuxt and Vuetify to build responsive and user-friendly interfaces.
+                On the backend, use .Net Core (v3,5,6) and Java SpringBoot (Gradle build) to develop robust and scalable applications.
               </li>
               <li>
-                Config CI/CD pipeline (Github Actions, Azure Devops), setting Docker, Helm, and Ingress files to deploy micro-services,
+                Handled CI/CD pipeline (Github Actions, Azure Devops), setting Docker, Helm, and Ingress files to deploy micro-services,
                 static web site/application on Azure/GCP Cloud and monitoring applications by using Grafana, Prometheus, Kibana, Elasticsearch, Fluentd.
               </li>
               <li>
-                Have experience work with building websites/site generators by using JamStack architect likes: Vuepress, Vitepress, Nuxt,
-                and Jekyll with deploy to some stuffs like Netlify, Vercel.
+                Created websites and site generators using JAMStack architecture. Used frameworks such as VuePress, VitePress, Nuxt, and Jekyll, and deployed them to platforms such as Netlify and Vercel.
               </li>
               <li>
-                Have experience work with use and config OAuth2.0 likes: Zoho CRM, Azure AD.
+                Worked with OAuth2.0 likes: Zoho CRM, Azure AD.
               </li>
               <li>
-                Have experience work with Astah diagrams for modelling model and draw diagram for applying architecture flow execution
+                Designed and modeled software systems and created diagrams that show the system architecture and execution flow accurately by using Astah.
               </li>
 
               {/*Familiar with using Linux, command, terminal.*/}
@@ -110,7 +106,7 @@ function App() {
             <ul className="cv-journey__experience-description">
               <li>Collaborated with a small team for training Ruby on Rails courses.
               </li>
-              <li>Have a change to learn about command line, Github, Gitlab, Linux.</li>
+              <li>Have a changed to learn about command line, Github, Gitlab, Linux.</li>
             </ul>
           </div>
           {/*intec*/}
@@ -121,7 +117,7 @@ function App() {
               Otc 2013 - Otc 2015 <span>// Ho Chi Minh, Vietnam</span>
             </div>
             <ul className="cv-journey__experience-description">
-              <li>Maintain and test outsource project with .Net stack: VB6, MVC 3.</li>
+              <li>Maintained and tested outsource project with .Net stack: VB6, MVC 3.</li>
             </ul>
           </div>
         </div>
