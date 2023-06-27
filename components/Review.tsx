b28762767f6fa2cc3bc3ae09394bb75960b739fd
vienpham <viendev@outlook.com>
2023-06-27 10:00:26 +0700

    feat: add comp Review

---
+++ components/Review.tsx
@@ -0,0 +1,11 @@
+import "./Review.css"
+
+export default function Review() {
+  return (
+    <>
+      <div className={'cv-review-popup'}>
+        {/*<p>Overall, are you satisfied with this CV? Do you think it reflects to your look up accurately and clearly? Is there anything you would like to add or change in my CV? Please reply to this email with your suggestion. I'm look forward to hearing from you soon.</p>*/}
+      </div>
+    </>
+  )
+}
