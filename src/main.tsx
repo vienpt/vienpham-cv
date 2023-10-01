43411311f4ebb509cc02901a9b70a0e17f91cd25
vienpham <viendev@outlook.com>
2023-10-01 21:41:33 +0700

    fix: clean main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import Review from "../components/Review.tsx";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    {/*<App>*/}
-    {/*  <Review isReview={false}/>*/}
-    {/*</App>*/}
   </React.StrictMode>,
 )
