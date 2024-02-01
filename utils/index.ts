374827b22d3e6bd1f8635e3b2a0f7eab0b1e5c85
vienpham <viendev@outlook.com>
2024-02-01 14:45:06 +0700

    fix: update condition check empty end date

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,8 @@
 function convertWorkingTime(date: string) {
+  if (!date) {
+    return 'Current'
+  }
+
   const parseDate = Date.parse(date)
   return new Date(parseDate).toLocaleDateString('en-US', {
     year: 'numeric', month: 'long'
